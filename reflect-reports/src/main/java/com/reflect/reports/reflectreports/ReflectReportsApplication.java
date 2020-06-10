a700b51ddcbaf36de715aea57ab3d078bad80c60
unknown <ctsjavauser180@iiht.tech>
2020-06-10 11:14:56 +0530

    Initial version of reports module

---
+++ reflect-reports/src/main/java/com/reflect/reports/reflectreports/ReflectReportsApplication.java
@@ -0,0 +1,13 @@
+package com.reflect.reports.reflectreports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReflectReportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReflectReportsApplication.class, args);
+	}
+
+}
---
+++ reflect-reports/src/test/java/com/reflect/reports/reflectreports/ReflectReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reflect.reports.reflectreports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReflectReportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
