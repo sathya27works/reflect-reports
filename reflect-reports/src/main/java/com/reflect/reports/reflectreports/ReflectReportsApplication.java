c2a38db51d3987649582be664559d59a6dac3743
unknown <ctsjavauser180@iiht.tech>
2020-06-11 21:16:11 +0530

    Replacing Reactive Mongo db with Mongo db dependency to resolve the bean

--- reflect-reports/src/main/java/com/reflect/reports/reflectreports/ReflectReportsApplication.java
+++ reflect-reports/src/main/java/com/reflect/reports/reflectreports/ReflectReportsApplication.java
@@ -2,10 +2,8 @@ package com.reflect.reports.reflectreports;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.reflect"})
 public class ReflectReportsApplication {
 
 	public static void main(String[] args) {
